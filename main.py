9c33296fe1a9ae8e4efd792ddd43b84ea5b6c94a
IsaqueNoll <isaque.noll@gmail.com>
2021-01-19 16:59:34 -0300

    Style: rename wav to mp3

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #imports
 import pdf_to_str as pts
-import str_to_wav as stw
+import str_to_mp3 as stm
 
 
 
 #def
 def writePdf(filename):
     text = pts.readPdf(filename)
-    stw.save(text)
+    stm.save(text)
